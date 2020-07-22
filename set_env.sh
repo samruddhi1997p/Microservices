ee45f2f402a241ec6817f8896c333e2381c12add
samruddhi1997p <samruddhi1997p@gmail.com>
2020-07-22 00:09:51 -0400

    fix

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagramsam
-export POSTGRES_PASSWORD=udagramsam
-export POSTGRES_HOST=udagramsam.cnzitabos3my.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagramsam
+export POSTGRESS_USERNAME=udagramsam
+export POSTGRESS_PASSWORD=udagramsam
+export POSTGRESS_HOST=udagramsam.cnzitabos3my.us-east-2.rds.amazonaws.com
+export POSTGRESS_DB=udagramsam
 export AWS_BUCKET=udagramsam
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
